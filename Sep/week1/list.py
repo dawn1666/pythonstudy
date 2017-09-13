5a44967cf93c4c87c62081a45dc07983832de33b
dawn1666 <402184660@qq.com>
2017-09-13 22:38:43 +0800

    list

---
+++ Sep/week1/list.py
@@ -0,0 +1,12 @@
+#!/usr/bin env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   Author：       Dawn Yan 
+   Email：       402184660@qq.com
+   date：          2017/9/13
+-------------------------------------------------
+"""
+__author__ = 'Dawn Yan'
+a=['Yan','Chen','Zhou','Liu','Qian']
+print(a[1:-1])
